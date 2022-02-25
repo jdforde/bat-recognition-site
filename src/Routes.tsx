cd5664eed148953f3363f0a01118625053bca177
mlewis0064 <61430932+mlewis0064@users.noreply.github.com>
2022-02-25 10:21:18 -0700

    Update Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import App from './pages/App';
 import EmailResults from './pages/EmailResults/EmailResults';
 import VideoUpload from './pages/Submission/VideoUpload';
+import Results from './pages/Results/Results';
 
 const Router = () => {
 
@@ -10,8 +11,10 @@ const Router = () => {
             <Route path="/" element={<App />}/>
             <Route path="/email-results" element={<EmailResults/>}/>
             <Route path="/submission" element={<VideoUpload/>}/>
+            <Route path="/results" element={<Results/>}/> 
         </Routes>
     )
 }
 
 export default Router;
+
