f29eff8fa82e5b0d49563817ac017fad823b4e3f
mlewis0064 <61430932+mlewis0064@users.noreply.github.com>
2022-02-25 10:22:47 -0700

    Update App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import logo from './graph.png';
-import banner from './banner.png';
+import banner from '../util/banner.jpeg';
 
 import {Link } from "react-router-dom";
 
@@ -9,23 +8,22 @@ import {Link } from "react-router-dom";
 function App() {
 
   return (
-    <div id ="c">
-    	<div id="header">
-        	<img src={banner} alt="McDowell Sonoran Conversancy" id="banner" width="100%" height ="220px"></img><br /><br /><br />
+    <div className="image">
+    	    	<div id="header">
+        	<img src={banner} alt="McDowell Sonoran Conversancy" id="banner" width="100%" height ="220px"></img><br />
+        	<div id="links2">
+        	<Link to="/">Home Page</Link>
+        	<Link to="/submission"> Upload Video </Link>
+        	<Link to="/email-results"> Get Email Results </Link>
+        	<Link to="/results"> Previous Results </Link>
+        	<a href="https://www.mcdowellsonoran.org/">Conservancy Home</a>
+        	</div>
         </div><br /><br />
         
 		<div id="wrapper">
         	<div id="main">
-        		<div id="link" className="link1"><Link to="/submission"> Upload Video </Link>
-        			<div className="desc1">Upload survey videos to analyze and record data to Sheets. </div></div>
-        		<div className="link2"><Link to="/email-results"> Get Email Results </Link>
-        			<div className="desc2">Upload one video at a time for a wait-free email with results when processing is complete.</div></div>
-        		<div className="link3"><Link to="/"> Previous Results </Link>
-        			<div className="desc3">View previous results, graphs, and other visual displays of the data. </div></div>
-        		<div id="link" className="link4"><a href="https://www.mcdowellsonoran.org/">Conservancy Home</a>
-					<div className="desc4">Visit the McDowell Sonoran Conservancy's homepage. </div></div>
-			</div>
-        </div>
+
+        </div></div>
     </div>
   );
 }
