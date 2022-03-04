91a5b241dfd178ffeb8296eed6515a2ac3b01fe7
Jakob Forde <jdforde@asu.edu>
2022-03-04 14:03:30 -0700

    Mulitthreaded testing and added 2 more test cases

--- python/tests/count_unittest.py
+++ python/tests/count_unittest.py
@@ -1,40 +1,56 @@
-import unittest
-
-#TODO: investigate multithreading so running all the tests does not take so long, gather a few more cases
+from concurrent.futures import ThreadPoolExecutor
+import time
 
 def placeholder(video_url):
-    return 0
+    return (0, 0)
 
 #
 # Test cases to test functionality of counting algorithm. Clips are short (<45 seconds) and should each
-# demonstrate something (different) that can happen in a complete video 
+# demonstrate something (different) that can happen in a complete video, Note that it's (num_enetered, num_exited)
 #
 
-class TestCountAlgorithm(unittest.TestCase):
+def TestVideo(video_num, count_tuple):
+    start_time = time.time()
+    response = placeholder('videos/' + str(video_num) + '.mp4')
+    if (response == count_tuple):
+        print('SUCCESS: Test case {} passed'.format(video_num))
+        print('Finished test case {} in {:.2f} seconds'.format(video_num, time.time() - start_time))
+        return True
+    else:
+        print('FAILURE: Test case {} did not pass. Expected {} but got {}'.format(video_num, count_tuple, response))
+        print('Finished test case {} in {:.2f} seconds'.format(video_num, time.time() - start_time))
+        return False
+    
+    
 
-    #There was some playing of the bats by the door, but only 1 left
-    def test_video1(self):
-        self.assertEqual(placeholder('test_videos/1.mp4'), -1)
+def ThreadedTests():
+    start_time = time.time()
+    print('=========================\nRunning Test Cases:\n')
+
+    # CORRECT_RESPONSES represents the tuples that the algorithm should get for each video. For 
+    # example (0, 1) corresponds to test video 4 where 0 bats entered and 1 exited
+    CORRECT_RESPONSES = [(0, 1), (0, 0), (0, 0), (0, 1), (0, 2), (1, 1), (1, 0), (0, 1)] 
+    NUM_WORKERS = 4 #Might want to experiment with this
+
+    futures = []
+    with ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
+        for i in range(1, 7):
+            future = executor.submit(TestVideo, i, CORRECT_RESPONSES[i-1])
+            futures.append(future)
     
-    #There was a lot of playing by multiple bats
-    def test_video2(self):
-        self.assertEqual(placeholder('test_videos/2.mp4'), 0)
+
+    print('\n\n\n=========================\nFinal Results:\n')
+    failure = False
+    for res in futures:
+        if res.result() == False:
+            print('FAILURE: some test cases did not pass')
+            failure = True
+            break
     
-    #A fox walks in and out of the region of interest
-    def test_video3(self):
-        self.assertEqual(placeholder('test_videos/3.mp4'), 0)
+    if not failure:
+        print('SUCCESS: All test cases passed!')
     
-    #One bat leaves by going in and out multiple times
-    def test_video4(self):
-        self.assertEqual(placeholder('test_videos/4.mp4'), -1)
-    
-    #The two leave with some playing, in and out region of interest
-    def test_video5(self):
-        self.assertEqual(placeholder('test_vidoes/5.mp4'), -2)
-    
-    #1 enters and 1 exits, so the counter should even itself out
-    def test_video6(self):
-        self.assertEqual(placeholder('test_videos/6.mp4'), 0)
-     
+    print('Finished running all test cases in {:.2f} seconds\n\n'.format(time.time() - start_time))
+        
 if __name__ == '__main__':
-    unittest.main()
+    ThreadedTests()
